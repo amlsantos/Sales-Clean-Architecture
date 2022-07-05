da18efd3223e79db4f7f8c890f35ed148e09a19e
andremlsantos <andremlsantos@gmail.com>
2022-07-05 18:11:01 +0100

    add api

--- Api/Customers/CustomersController.cs
+++ Api/Customers/CustomersController.cs
@@ -1,19 +1,34 @@
-﻿using Application.Customers.Queries.GetCustomerList;
+﻿using System.Net;
+using Application.Customers.Commands.CreateCustomer;
+using Application.Customers.Queries.GetCustomerList;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers;
+namespace Api.Customers;
 
 [ApiController]
 [Route("[controller]")]
 public class CustomersController : ControllerBase
 {
     private readonly IGetCustomersListQuery _query;
+    private readonly ICreateCustomerCommand _command;
 
-    public CustomersController(IGetCustomersListQuery query) => _query = query;
+    public CustomersController(IGetCustomersListQuery query, ICreateCustomerCommand command)
+    {
+        _query = query;
+        _command = command;
+    }
 
     [HttpGet]
     public IEnumerable<CustomerModel> Get()
     {
         return _query.Execute();
+    }
+
+    [HttpPost]
+    public HttpResponseMessage Create(CreateCustomerModel model)
+    {
+        _command.Execute(model);
+        
+        return new HttpResponseMessage(HttpStatusCode.Created);
     }
 }
